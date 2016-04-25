a49609ce02a05b223eb451f6aa14b2142f7216b8
Matt Knox <matt.knox@sphero.com>
2016-04-25 15:34:57 -0600

    Fix

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -818,7 +818,7 @@ params = CGI.parse(uri.query || "")
       { "DATABASE_URL" => database_url }
     else
       {}
-    env.merge(user_env_hash)
+    end.merge(user_env_hash)
   end
 
   def database_url
